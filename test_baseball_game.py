8f5868f12e58a6b7c74a38e0eea89a1fee6d2e58
dayae001 <dayae001@naver.com>
2024-06-13 13:54:18 +0900

    [refactoring] test_exception_when_invalid_input

--- test_baseball_game.py
+++ test_baseball_game.py
@@ -5,16 +5,17 @@ from baseball_game import BaseballGame
 
 class TestBaseballGame(TestCase):
     def setUp(self):
-        game = BaseballGame()
+        self.game = BaseballGame()
         super().setUp()
 
-    def test_exception_when_input_is_none(self):
-        self.game = BaseballGame()
-        with self.assertRaises(TypeError):
-            self.game.guess(None)
+    def assert_illegal_argument(self, guessNumber):
+        try:
+            self.game.guess(guessNumber)
+            self.fail()
+        except TypeError:
+            pass
 
-    def test_exception_when_input_length_is_unmatched(self):
-        self.game = BaseballGame()
-        with self.assertRaises(TypeError):
-            self.game.guess(12)
-
+    def test_exception_when_invalid_input(self):
+        self.assert_illegal_argument(None)
+        self.assert_illegal_argument("12")
+        self.assert_illegal_argument("1234")
