8277c6ec7c82d2c32aa327da7b919c147403f9b8
dayae001 <dayae001@naver.com>
2024-06-13 13:48:00 +0900

    [feature] test_exception_when_input_length_is_unmatched

--- test_baseball_game.py
+++ test_baseball_game.py
@@ -4,7 +4,17 @@ from baseball_game import BaseballGame
 
 
 class TestBaseballGame(TestCase):
+    def setUp(self):
+        game = BaseballGame()
+        super().setUp()
+
     def test_exception_when_input_is_none(self):
         self.game = BaseballGame()
         with self.assertRaises(TypeError):
             self.game.guess(None)
+
+    def test_exception_when_input_length_is_unmatched(self):
+        self.game = BaseballGame()
+        with self.assertRaises(TypeError):
+            self.game.guess(12)
+
